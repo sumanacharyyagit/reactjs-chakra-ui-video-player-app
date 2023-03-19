751ef1b934d5d1c3dd3b3f16e4d34c0f1cf0f506
sumanacharyyagit <sumanacharyya999@gmail.com>
2023-03-20 04:54:05 +0530

    Images update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -90,25 +90,25 @@ const MyCarousel = () => (
     showArrows={false}
   >
     <Box w={'full'} h={'100vh'}>
-      <Image src={img1} />
+      <Image src={img1} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         The Meta World
       </Heading>
     </Box>
     <Box w={'full'} h={'100vh'}>
-      <Image src={img2} />
+      <Image src={img2} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Future Console
       </Heading>
     </Box>
     <Box w={'full'} h={'100vh'}>
-      <Image src={img3} />
+      <Image src={img3} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Closeup to the Console
       </Heading>
     </Box>
     <Box w={'full'} h={'100vh'}>
-      <Image src={img4} />
+      <Image src={img4} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Going to the Future
       </Heading>
